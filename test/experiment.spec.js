6d1fc229abfc5fa89b672ab8cf9875bb05c97108
Sune Simonsen <sune@we-knowhow.dk>
2017-11-26 23:31:33 +0100

    Added a demo file

---
+++ test/experiment.spec.js
@@ -0,0 +1,25 @@
+/*global describe, it, beforeEach*/
+const Generators = require('chance-generators');
+const expect = require('unexpected');
+
+const demo = require('../lib/demo');
+
+expect.use(require('../lib/unexpected-check'));
+
+const { natural, string } = new Generators(42);
+
+describe('unexpected-check', () => {
+    it('does not find the secret', () => {
+        expect((text) => {
+            demo.findTheSecret(text);
+        }, 'to be valid for all', string({ length: natural({ max: 50 }) }));
+    });
+
+    it('does not find magic numbers', () => {
+        const numbers = natural({ max: 200 });
+
+        expect((a, b, c) => {
+            demo.findTheMagicNumbers(a, b, c);
+        }, 'to be valid for all', numbers, numbers, numbers);
+    });
+});
