bcd3b9dafacca74262b19906a6d0d8d1db278b4d
Sune Simonsen <sune@we-knowhow.dk>
2017-11-26 23:58:14 +0100

    wip

--- test/experiment.spec.js
+++ test/experiment.spec.js
@@ -11,7 +11,15 @@ const { natural, string } = new Generators(42);
 describe('unexpected-check', () => {
     it('does not find the secret', () => {
         expect((text) => {
-            demo(text);
+            demo.findTheSecret(text);
         }, 'to be valid for all', string({ length: natural({ max: 50 }) }));
+    });
+
+    it('does not find magic numbers', () => {
+        const numbers = natural({ max: 200 });
+
+        expect((a, b, c) => {
+            demo.findTheMagicNumbers(a, b, c);
+        }, 'to be valid for all', numbers, numbers, numbers);
     });
 });
